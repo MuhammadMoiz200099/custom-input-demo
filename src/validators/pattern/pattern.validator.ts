4b852e798bb1cf22599d167d7cd85a755ee4058f
muhammadmoiz200099 <muhammadmoiz0087@gmail.com>
2022-05-30 09:35:00 -0400

    updates

--- src/validators/pattern/pattern.validator.ts
+++ src/validators/pattern/pattern.validator.ts
@@ -1,13 +1,16 @@
 import { Validator } from '../validator';
 
-const DEFAULT_PATTERN = "(https|http|ftp):\/\/|(\w+\.|www.|(\w+:))(\w+)|(\/|-|_|/+|%|&|/+%)(\w+)|\/?\w+|=\w+|(\/)|(\.\w+)";
+const DEFAULT_PATTERN = /(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-F\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,4}(:[\d]+)?(\/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?/;
 
 export function patternValidator(pattern: string): Validator<string> {
     return {
         validate: (value: string) => {
-          const reg_pattern = pattern ? pattern : DEFAULT_PATTERN;
-          const regex = new RegExp(reg_pattern);
-          return regex.test(value);
+          if(pattern) {
+            const regex = new RegExp(pattern);
+            return regex.test(value);
+          } else {
+            return DEFAULT_PATTERN.test(value);
+          }
         },
         errorMessage: 'Fill in a valid URL'
     };
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'arvato-test',
+  namespace: 'custom-input-demo',
   outputTargets: [
     {
       type: 'dist',
